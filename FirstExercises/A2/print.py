9832395e1d716f79236fc3aa0ee0e799c82674b8
Gerardo ValQ <A01374994@itesm.mx>
2020-10-01 20:03:30 -0500

    Create print.py

---
+++ FirstExercises/A2/print.py
@@ -0,0 +1,14 @@
+  
+#encoding: UTF-8
+
+# Autor: Gerardo Arturo Valderama Quiroz, A01374994
+# Descripcion: Presentar mis datos personales con ayuda de un programa 
+
+# A partir de aquí escribe tu programa
+
+print("Nombre: Gerardo Arturo Valderrama Quiroz")
+print("Matricula: A01374994")
+print("Carrera: ISC - Ingeneria en Sistemas Computacionales")
+print("Escuela de procedencia: Prepa Tec Bicultural")
+print("""Descripcion: Me gusta mucho leer, sobretodo de fantasia, ciencia ficción y aventura, los videojuegos,
+             mi favorito es The legend of Zelda, Me gusta la comida de todo el mundo y me gusta cocinar.""")
